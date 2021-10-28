28595e45e351113c0ca71df43e69dcfe7b01c025
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2021-10-28 21:23:06 +0200

    Added mosquito image

---
+++ src/components/Mosquito.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/index.css';
+
+
+function Mosquito() {
+    return (
+        <div className="mosquito-img">
+            
+        </div>
+    )
+}
+
+export default Mosquito
