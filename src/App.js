8837ce31e3f5e057376cd76cb7b4ec13af02e227
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2021-10-28 21:40:54 +0200

    Added mosquito image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components/Button";
+import Mosquito from "./components/Mosquito";
 
 function App() {
   return (
     <div className="App">
       <h2>How many mosquitoes can you catch in 15 seconds?</h2>
       <Button title="Play again"></Button>
+      <Mosquito/>
     </div>
   );
 }
