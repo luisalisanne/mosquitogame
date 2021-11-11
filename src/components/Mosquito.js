3cbe3a6534b9492f2e4f5706d780b006eb5c7800
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2021-11-11 21:24:57 +0100

    Add mosquito elements with random position

--- src/components/Mosquito.js
+++ src/components/Mosquito.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import '../index.css';
-
-
 function Mosquito() {
+
+    const mosquitos = [...Array(20)]
+    
     return (
-        <div className="mosquito-img">
-            
+        <div>
+            {mosquitos.map((i, index) => {return <div key={index} className="mosquito-img" style={{left:`${Math.floor(Math.random()*(window.innerWidth))}px`, top:`${Math.floor(Math.random()*(window.innerHeight)+200)}px`, position:'relative'}}></div>})}
         </div>
     )
 }
