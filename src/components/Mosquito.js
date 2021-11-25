675b3a6aa4363d72acbf125953be32a66b08f4e6
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2021-11-25 20:51:08 +0100

    Change mosquito style, add keyframes animation

--- src/components/Mosquito.js
+++ src/components/Mosquito.js
@@ -1,12 +1,23 @@
 function Mosquito() {
+  const mosquitos = [...Array(20)];
 
-    const mosquitos = [...Array(20)]
-    
-    return (
-        <div>
-            {mosquitos.map((i, index) => {return <div key={index} className="mosquito-img" style={{left:`${Math.floor(Math.random()*(window.innerWidth))}px`, top:`${Math.floor(Math.random()*(window.innerHeight)+200)}px`, position:'relative'}}></div>})}
-        </div>
-    )
+  return (
+    <div>
+      {mosquitos.map((i, index) => {
+        return (
+          <div
+            key={index}
+            className="mosquito-img mosquito-animation"
+            style={{
+              left: `${Math.floor(Math.random() * window.innerWidth)}px`,
+              top: `${Math.floor(Math.random() * window.innerHeight + 200)}px`,
+              position: "relative",
+            }}
+          ></div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Mosquito
+export default Mosquito;
