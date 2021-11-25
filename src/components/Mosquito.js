c2060457c8da41cae4c83d9b651df45a2741172d
Kristin Fritsch <ich@kristin-fritsch.de>
2021-11-25 21:31:59 +0100

    Don't update mosquito styles on re-render, fixes #13

--- src/components/Mosquito.js
+++ src/components/Mosquito.js
@@ -1,15 +1,18 @@
-function Mosquito({ onClick }) {
+import { useRef } from "react";
+
+const Mosquito = ({ onClick }) => {
+  const mosquitoStyles = useRef({
+    left: `${Math.floor(Math.random() * window.innerWidth)}px`,
+    top: `${Math.floor(Math.random() * window.innerHeight + 200)}px`,
+    position: "relative",
+  });
   return (
     <div
       onClick={onClick}
       className="mosquito-img mosquito-animation"
-      style={{
-        left: `${Math.floor(Math.random() * window.innerWidth)}px`,
-        top: `${Math.floor(Math.random() * window.innerHeight + 200)}px`,
-        position: "relative",
-      }}
+      style={mosquitoStyles.current}
     ></div>
   );
-}
+};
 
 export default Mosquito;
